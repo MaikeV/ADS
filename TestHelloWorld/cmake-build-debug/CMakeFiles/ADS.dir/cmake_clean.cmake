35d98860370359823b1fbeb9f94889b72761eb65
Maike <maike.voss7@gmail.com>
2019-04-01 14:45:11 +0200

    Folder Structure and initial commit

---
+++ TestHelloWorld/cmake-build-debug/CMakeFiles/ADS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ADS.dir/main.cpp.obj"
+  "ADS.pdb"
+  "ADS.exe"
+  "ADS.exe.manifest"
+  "libADS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ADS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
